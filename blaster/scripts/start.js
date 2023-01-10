701ac5cedf8494eecee8ffde4ec53499d9ec68f1
hubert-kniola <hubert.kniola@gmail.com>
2023-01-10 21:34:44 +0100

    blaster

---
+++ blaster/scripts/start.js
@@ -0,0 +1,19 @@
+let _started = false;
+let _sound = document.getElementById("checkbox").checked;
+
+function startGame() {
+  let start = document.getElementById("game-main");
+  let holder = document.getElementById("holder");
+
+  start.classList.remove("hide");
+
+  holder.style.display = "none";
+  holder.classList.add("hide");
+
+  let audio = new Audio("sounds/soundtrack.wav");
+  let audio_start = new Audio("sounds/start.wav");
+  if (_sound) {
+    audio.play();
+    audio_start .play();
+  }
+}
